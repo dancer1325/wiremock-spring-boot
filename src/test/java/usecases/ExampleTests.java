ed4730a43be76c2c47fa761475e3b0016fa17fd4
dancer1325 <alfredotic0809@gmail.com>
2025-03-18 13:28:52 +0100

    doc(test.java.useCases): add example

---
+++ src/test/java/usecases/ExampleTests.java
@@ -0,0 +1,33 @@
+package usecases;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestClient;
+import org.wiremock.spring.EnableWireMock;
+
+@SpringBootTest(classes = ExampleTests.AppConfiguration.class)
+@EnableWireMock
+public class ExampleTests {
+  @Value("${wiremock.server.baseUrl}")
+  private String wireMockUrl;
+
+  @Test
+  void returns_a_ping() {
+    stubFor(get("/ping").willReturn(ok("pong")));
+
+    RestClient client = RestClient.create();
+    String body = client.get().uri(wireMockUrl + "/ping").retrieve().body(String.class);
+
+    assertThat(body).isEqualTo("pong");
+  }
+
+  @SpringBootApplication
+  static class AppConfiguration {}
+}
